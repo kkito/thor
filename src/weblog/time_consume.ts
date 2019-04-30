88018102ee04b0c41772ce768ef37e0a5453e773
kk <kk@littlelights.ai>
2019-04-30 19:17:40 +0800

    set timeout for TimeConsume

--- src/weblog/time_consume.ts
+++ src/weblog/time_consume.ts
@@ -1,21 +1,34 @@
 import { Weblog, IAnyStringKeyObject } from './index'
 export class TimeConsume {
   protected logger: Weblog
-  protected startTime:Date
-  protected params:IAnyStringKeyObject
+  protected startTime: Date
+  protected params: IAnyStringKeyObject
+  protected timer: any = null
 
-
-  constructor(logger: Weblog, params:IAnyStringKeyObject = {}) {
+  constructor(logger: Weblog, params: IAnyStringKeyObject = {}, timeout = 5000) {
     this.logger = logger
     this.startTime = new Date()
     this.params = params
+    this.timer = setTimeout(() => {
+      this._auto_finish()
+    }, timeout)
   }
 
-  finish(status=200):IAnyStringKeyObject {
+  _auto_finish(): void {
+    if (this.timer) {
+      const cost = new Date().getTime() - this.startTime.getTime()
+      this.params['G_time_consuming'] = cost
+      this.params['G_status'] = -1
+      this.logger.send({ G_time_cost: this.params })
+    }
+  }
+
+  finish(status = 200): IAnyStringKeyObject {
     const cost = new Date().getTime() - this.startTime.getTime()
     this.params['G_time_consuming'] = cost
     this.params['G_status'] = status
-    this.logger.send({'G_time_cost': this.params})
+    this.logger.send({ G_time_cost: this.params })
+    this.timer = null
     return this.params
   }
 }
